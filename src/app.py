29843e0cce47d6d62069c3a540a54fbc8ae50a9e
Gonzalo Osco Hernadez <goh.acm@gmail.com>
2026-01-09 20:36:44 -0400

    try

--- src/app.py
+++ src/app.py
@@ -6,23 +6,30 @@ s3_client = boto3.client('s3')
 BUCKET_NAME = os.environ['BUCKET_NAME']
 
 def lambda_handler_upload(event, context):
-    body = json.loads(event.get('body', '{}'))
-    file_name = body.get('filename', 'upload_default')
-    
-    # pre signed URL for PUT
-    presigned_url = s3_client.generate_presigned_url(
-        'put_object',
-        Params={'Bucket': BUCKET_NAME, 'Key': file_name},
-        ExpiresIn=300 # 5 minutes
-    )
-    
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "objectKey": file_name,
-            "uploadUrl": presigned_url
-        })
-    }
+    try: 
+        body = json.loads(event.get('body', '{}'))
+        file_name = body.get('filename', 'upload_default')
+        
+        # pre signed URL for PUT
+        presigned_url = s3_client.generate_presigned_url(
+            'put_object',
+            Params={'Bucket': BUCKET_NAME, 'Key': file_name},
+            ExpiresIn=300 # 5 minutes
+        )
+        
+        return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "objectKey": file_name,
+                "uploadUrl": presigned_url
+            })
+        }
+    except Exception as e:
+        print(f"Error: {str(e)}") #  CloudWatch
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": "Error interno del servidor", "details": str(e)})
+        }
 
 
 def lambda_handler_download(event, context):
