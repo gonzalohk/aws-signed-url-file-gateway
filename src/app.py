6e53d39621ed18550bc3c35f1e9fb02c5ad33c6e
Gonzalo Osco Hernadez <goh.acm@gmail.com>
2026-01-09 20:11:00 -0400

    lambda function to upload and download

---
+++ src/app.py
@@ -0,0 +1,43 @@
+import boto3
+import os
+import json
+
+s3_client = boto3.client('s3')
+BUCKET_NAME = os.environ['BUCKET_NAME']
+
+def lambda_handler_upload(event, context):
+    body = json.loads(event.get('body', '{}'))
+    file_name = body.get('filename', 'upload_default')
+    
+    # pre signed URL for PUT
+    presigned_url = s3_client.generate_presigned_url(
+        'put_object',
+        Params={'Bucket': BUCKET_NAME, 'Key': file_name},
+        ExpiresIn=300 # 5 minutes
+    )
+    
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "objectKey": file_name,
+            "uploadUrl": presigned_url
+        })
+    }
+
+
+def lambda_handler_download(event, context):
+    object_key = event['pathParameters']['objectKey']
+    
+    # pre signed URL for GET
+    download_url = s3_client.generate_presigned_url(
+        'get_object',
+        Params={'Bucket': BUCKET_NAME, 'Key': object_key},
+        ExpiresIn=3600 # 1 hour
+    )
+    
+    return {
+        "statusCode": 307,
+        "headers": {
+            "Location": download_url
+        }
+    }
